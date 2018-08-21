c82f28b3b6a8a390833cbaa5204226059cdee98d
Ma'ruf Shidiq <maruf.shidiq@mail.ugm.ac.id>
2018-08-21 12:56:43 +0700

    Add LCD

--- mentor_v1.ino
+++ mentor_v1.ino
@@ -1,9 +1,13 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
 #include "MServo.h"
 #include "variable.h"
 
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
 MServo* axis = new MServo[numAxis];
 
 void setup() {
+  lcd.begin(16,2);
   for (int n = 0; n < numAxis; n++) {
     axis[n].attach(potPin[n], dirPin[n], pwmPin[n]);
     axis[n].setAngleConstrain(minAngle[n], maxAngle[n]);
@@ -22,4 +26,17 @@ void loop() {
       axis[BASE].write(target);
     }
   }
+}
+
+void lcd_print(uint8_t x, uint8_t y, const char* fmtstr, ...)
+{
+  char lcd_string[21];
+  va_list ap;
+
+  va_start(ap, fmtstr);
+  vsprintf(lcd_string, fmtstr, ap);
+  va_end(ap);
+
+  lcd.setCursor(x, y);
+  lcd.print(lcd_string);
 }
