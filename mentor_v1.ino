f9eb37a980f9bcf73767ace089afeffdbad60f52
Ma'ruf Shidiq <maruf.shidiq@mail.ugm.ac.id>
2018-08-24 10:46:56 +0700

    Add calibrate system

--- mentor_v1.ino
+++ mentor_v1.ino
@@ -1,5 +1,6 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <EEPROM.h>
 #include "MServo.h"
 #include "MButton.h"
 #include "menu.h"
@@ -15,25 +16,20 @@ void setup() {
   btn.attach(btnPin, INPUT_PULLUP);
   btn.setState(btnState);
   btn.setActive(LOW);
+  loadEEPROM();
   for (int n = 0; n < numAxis; n++) {
     axis[n].attach(potPin[n], dirPin[n], pwmPin[n]);
     axis[n].setAngleConstrain(minAngle[n], maxAngle[n]);
     axis[n].setPotConstrain(minPot[n], maxPot[n]);
   }
+  /* Reverse the Elbow direction */
+  axis[ELBOW].reverse = true;
   axis[BASE].write(100);
   Serial.begin(115200);
 }
 
 void loop() {
   mainMenu();
-  Serial.println(axis[BASE].movingAverage());
-  axis[BASE].update();
-  if (Serial.available() > 0) {
-    if (Serial.find("m")) {
-      int target = Serial.parseInt();
-      axis[BASE].write(target);
-    }
-  }
 }
 
 void lcd_print(uint8_t x, uint8_t y, const char* fmtstr, ...)
@@ -72,7 +68,10 @@ void mainMenu() {
 }
 
 void runSystem() {
-
+  lcd.clear();
+  lcd.print("Running");
+  while (1) {
+  }
 }
 
 void calibrateMenu() {
@@ -83,9 +82,11 @@ void calibrateMenu() {
     index = btn.getIndex(5);
     showMenu(lcd, nameAxis, index);
     if (btn.getBtn() == OK) {
+      while (btn.getBtn() == OK) {}
       calibrateAxis(index);
     }
     else if (btn.getBtn() == CANCEL) {
+      while (btn.getBtn() == CANCEL) {}
       break;
     }
   }
@@ -93,23 +94,97 @@ void calibrateMenu() {
 
 void calibrateAxis(int num) {
   lcd.clear();
+  int set = 0;
   while (1) {
     lcd.setCursor(0, 0);
     lcd.print("Calibrate ");
     lcd.print(nameAxis[num]);
-    lcd_print(0, 1, "<    %4d      >", axis[num].movingAverage());
-
     if (btn.getBtn() == CANCEL) {
+      while (btn.getBtn() == CANCEL) {}
       break;
     }
-    else if (btn.getBtn() == UP) {
+    else if (btn.getBtn() == OK) {
+      while (btn.getBtn() == OK) {}
+      calibrateAxisConstrain(num, set);
+    }
+    lcd.setCursor(0, 1);
+    if (set == 0) {
+      lcd.print("~");
+    }
+    else {
+      lcd.print(" ");
+    }
+    lcd.print("Min");
+
+    lcd.setCursor(8, 1);
+    if (set == 1) {
+      lcd.print("~");
+    }
+    else {
+      lcd.print(" ");
+    }
+    lcd.print("Max");
+
+    set = btn.getIndex(2);
+  }
+}
+
+void calibrateAxisConstrain(int num, int mode) { // Mode => 0 = Min ; 1 = Max
+  lcd.clear();
+  while (1) {
+    /* Display Section */
+    lcd.setCursor(0, 0);
+    lcd.print(nameAxis[num]);
+    if (mode == 0) {
+      lcd.print(" MIN");
+    }
+    else if (mode == 1) {
+      lcd.print(" MAX");
+    }
+    lcd_print(0, 1, "<    %4d      >", axis[num].movingAverage());
+
+    /* Button Section */
+    if (btn.getBtn() == UP) {
       axis[num].move(200);
     }
     else if (btn.getBtn() == DOWN) {
       axis[num].move(-200);
     }
+    else if (btn.getBtn() == CANCEL) {
+      while (btn.getBtn() == CANCEL) {}
+      lcd.clear();
+      break;
+    }
+    else if (btn.getBtn() == OK) {
+      lcd.clear();
+      if (mode == 0) {
+        axisData[num].minPot = axis[num].movingAverage();
+      }
+      else if (mode == 1) {
+        axisData[num].maxPot = axis[num].movingAverage();
+      }
+      lcd.print("Saving Data");
+      EEPROM.put(axisDataAddr, axisData);
+      for (int i = 1; i <= 5; i++) {
+        lcd.print(".");
+        delay(200);
+      }
+      lcd.clear();
+      break;
+    }
+
     if (!btn.isPressed()) {
       axis[num].move(0);
+    }
+  }
+}
+
+void loadEEPROM() {
+  EEPROM.get(axisDataAddr, axisData);
+  for (int n = 0; n < numAxis; n++) {
+    if ((axisData[n].minPot != 0) && (axisData[n].maxPot != 0)) {
+      minPot[n] = axisData[n].minPot;
+      maxPot[n] = axisData[n].maxPot;
     }
   }
 }
--- variable.h
+++ variable.h
@@ -18,10 +18,20 @@ const int dirPin[] = {  6,      10,        8,     4,     2};
 const int pwmPin[] = {  7,      11,        9,     5,     3};
 
 int minAngle[]     = {  0,       0,        0,     0,      0};
-int maxAngle[]     = {210,     180,      230,   320,    320};
+int maxAngle[]     = {210,     180,      210,   320,    320};
 
 int minPot[]       = { 10,      47,        0,     0,      0};
 int maxPot[]       = {935,     662,     1024,  1024,    1024};
 
 //int minPot[]       = {  0,       0,        0,     0,      0};
 //int maxPot[]       = {1024,    1024,     1024,  1024,    1024};
+
+struct AxisData {
+  int minPot;
+  int maxPot;
+};
+
+// EEPROM Address Allocation
+const int axisDataAddr = 50; // Start at 50; End at (50 + (AxisData * numAxis))
+
+AxisData axisData[numAxis];
